c7c2a231b45ee63dc2d2d6d6f09a8278c066d8f5
Panth Patel <panthhp@outlook.com>
2023-05-04 20:28:55 +0100

    util

--- lib/util/api.dart
+++ lib/util/api.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -8,6 +9,7 @@ FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 FirebaseStorage storage = FirebaseStorage.instance;
 FirebaseMessaging messaging = FirebaseMessaging.instance;
+FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
 // Constants
 String blankPicUrl = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
