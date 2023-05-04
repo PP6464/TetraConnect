e0b7263ee79f5a350bbc4c5577cf9bd307ffe441
Panth Patel <panthhp@outlook.com>
2023-05-04 20:29:07 +0100

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -62,6 +63,9 @@ class MyApp extends StatelessWidget {
           title: 'TetraConnect',
           debugShowCheckedModeBanner: false,
           localizationsDelegates: AppLocalizations.localizationsDelegates,
+          navigatorObservers: [
+            FirebaseAnalyticsObserver(analytics: analytics),
+          ],
           supportedLocales: const [
             Locale("en"),
           ],
