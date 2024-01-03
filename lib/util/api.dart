b9b94fe025db188ba916111f07a06816bd20a93e
Panth Patel <panthhp@outlook.com>
2024-01-03 20:45:28 +0000

    add api url

--- lib/util/api.dart
+++ lib/util/api.dart
@@ -13,4 +13,5 @@ FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
 // Constants
 String blankPicUrl = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
-String baseStorageURL = "https://firebasestorage.googleapis.com/v0/b/tetraconnect.appspot.com/o/";
+String baseStorageUrl = "https://firebasestorage.googleapis.com/v0/b/tetraconnect.appspot.com/o/";
+String apiUrl = "https://tetraconnect.onrender.com/";
