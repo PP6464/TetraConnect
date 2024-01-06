4ce5019062ad5b40fa748e6a9d084dccf0845913
Panth Patel <panthhp@outlook.com>
2024-01-06 20:53:50 +0000

    show and cancel pending friend requests

--- lib/pages/friends/pending.friend.requests.dart
+++ lib/pages/friends/pending.friend.requests.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:tetraconnect/util/api.dart';
 
 import '../../ui/elements.dart';
 import '../../util/route.dart';
@@ -36,6 +38,68 @@ class _PendingFriendRequestsState extends State<PendingFriendRequestsPage> {
                 style: const TextStyle(
                   fontSize: 25.0,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Container(
+                constraints: const BoxConstraints(
+                  maxHeight: 500.0,
+                  maxWidth: 500.0,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: firestore.collection("friends").where("from", isEqualTo: provider(context).user!.ref).snapshots(),
+                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      return asyncBuilder(
+                        context,
+                        snapshot,
+                        (data) {
+                          return data.docs.isNotEmpty
+                              ? ListView.builder(
+                                  itemCount: data.docs.length,
+                                  itemBuilder: (BuildContext context, int index) => StreamBuilder(
+                                    stream: (data.docs[index]["users"].singleWhere((element) => element.id != provider(context).user!.uid) as DocumentReference).snapshots(),
+                                    builder: (context, snapshot) => asyncBuilder(
+                                      context,
+                                      snapshot,
+                                      (userData) => Card(
+                                        child: ListTile(
+                                          leading: CircleAvatar(
+                                            radius: 25.0,
+                                            backgroundImage: NetworkImage(userData["photoUrl"]),
+                                          ),
+                                          title: Text(
+                                            userData["displayName"],
+                                            textScaler: TextScaler.linear(provider(context).tsf),
+                                          ),
+                                          subtitle: SingleChildScrollView(
+                                            scrollDirection: Axis.horizontal,
+                                            child: Text(
+                                              userData.id,
+                                              textScaler: TextScaler.linear(provider(context).tsf),
+                                            ),
+                                          ),
+                                          trailing: IconButton(
+                                            tooltip: AppLocalizations.of(context)!.deleteFriendRequest,
+                                            icon: const Icon(Icons.delete),
+                                            onPressed: () async {
+                                              await data.docs[index].reference.delete();
+                                            },
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              : Text(
+                                  AppLocalizations.of(context)!.noPendingRequests,
+                                  textScaler: TextScaler.linear(provider(context).tsf),
+                                );
+                        },
+                      );
+                    },
+                  ),
                 ),
               ),
             ],
