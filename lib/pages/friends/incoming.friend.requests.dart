cfd05c25855fe22d620f6ba0a04cfcc2b39c3e8e
Panth Patel <panthhp@outlook.com>
2024-01-06 21:16:57 +0000

    incoming friend requests

--- lib/pages/friends/incoming.friend.requests.dart
+++ lib/pages/friends/incoming.friend.requests.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:tetraconnect/util/api.dart';
 
 import '../../ui/elements.dart';
 import '../../util/route.dart';
@@ -36,6 +39,86 @@ class _IncomingFriendRequestPageState extends State<IncomingFriendRequestPage> {
                 style: const TextStyle(
                   fontSize: 25.0,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Container(
+                constraints: const BoxConstraints(
+                  maxWidth: 500.0,
+                  maxHeight: 500.0,
+                ),
+                child: StreamBuilder(
+                  stream: firestore.collection("friends").where("to", isEqualTo: provider(context).user!.ref).where("state", isEqualTo: "pending").snapshots(),
+                  builder: (context, snapshot) => asyncBuilder(
+                    context,
+                    snapshot,
+                    (data) {
+                      return data.docs.isEmpty
+                          ? Text(
+                              AppLocalizations.of(context)!.noIncomingRequests,
+                              textScaler: TextScaler.linear(provider(context).tsf),
+                            )
+                          : ListView.builder(
+                              itemCount: data.docs.length,
+                              itemBuilder: (BuildContext context, int index) => StreamBuilder<DocumentSnapshot>(
+                                stream: (data.docs[index]["from"] as DocumentReference).snapshots(),
+                                builder: (context, snapshot) => asyncBuilder(
+                                  context,
+                                  snapshot,
+                                  (userData) => Card(
+                                    child: ListTile(
+                                      leading: CircleAvatar(
+                                        radius: 25.0,
+                                        backgroundImage: NetworkImage(userData["photoUrl"]),
+                                      ),
+                                      title: Text(
+                                        userData["displayName"],
+                                        textScaler: TextScaler.linear(provider(context).tsf),
+                                      ),
+                                      subtitle: SingleChildScrollView(
+                                        scrollDirection: Axis.horizontal,
+                                        child: Text(
+                                          userData.id,
+                                          textScaler: TextScaler.linear(provider(context).tsf),
+                                        ),
+                                      ),
+                                      trailing: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          IconButton(
+                                            tooltip: AppLocalizations.of(context)!.accept,
+                                            onPressed: () async {
+                                              await Dio().patch(
+                                                "$apiUrl/friend-request/accept/${data.docs[index].id}",
+                                                queryParameters: {
+                                                  "acceptedBy": provider(context).user!.uid,
+                                                },
+                                              );
+                                            },
+                                            icon: const Icon(Icons.check),
+                                          ),
+                                          const SizedBox(width: 8.0),
+                                          IconButton(
+                                            tooltip: AppLocalizations.of(context)!.reject,
+                                            onPressed: () async {
+                                              await Dio().patch(
+                                                "$apiUrl/friend-request/reject/${data.docs[index].id}",
+                                                queryParameters: {
+                                                  "rejectedBy": provider(context).user!.uid,
+                                                },
+                                              );
+                                            },
+                                            icon: const Icon(Icons.clear),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                    },
+                  ),
                 ),
               ),
             ],
