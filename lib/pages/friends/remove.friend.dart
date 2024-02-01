80bdff951469deffc9cd3a4828498bfd9d756060
PP6464 <panthhp@outlook.com>
2024-02-01 11:30:38 +0000

    remove friend

--- lib/pages/friends/remove.friend.dart
+++ lib/pages/friends/remove.friend.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:tetraconnect/util/api.dart';
 
 import '../../ui/elements.dart';
 import '../../util/route.dart';
@@ -36,6 +39,75 @@ class _RemoveFriendPageState extends State<RemoveFriendPage> {
                 style: const TextStyle(
                   fontSize: 25.0,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              StreamBuilder(
+                stream: firestore
+                    .collection("friends")
+                    .where("state", isEqualTo: "accepted")
+                    .where("users", arrayContains: provider(context).user!.ref)
+                    .snapshots(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) => asyncBuilder(
+                  context,
+                  snapshot,
+                  (friends) {
+                    if (friends.docs.isEmpty) {
+                      return Text(
+                        AppLocalizations.of(context)!.noRemoveFriends,
+                        textScaler: TextScaler.linear(provider(context).tsf),
+                      );
+                    } else {
+                      return Container(
+                        constraints: const BoxConstraints(
+                          maxWidth: 500.0,
+                          maxHeight: 500.0,
+                        ),
+                        child: ListView.builder(
+                          itemCount: friends.docs.length,
+                          itemBuilder: (BuildContext context, int index) => Card(
+                            child: StreamBuilder(
+                              stream: (friends.docs[index]["users"] as List<DocumentReference>)
+                                  .singleWhere((element) => element.id != provider(context).user!.uid)
+                                  .snapshots(),
+                              builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) => asyncBuilder(
+                                context,
+                                snapshot,
+                                (friend) => Card(
+                                  child: ListTile(
+                                    leading: CircleAvatar(
+                                      radius: 25.0,
+                                      backgroundImage: NetworkImage(friend["photoUrl"]),
+                                    ),
+                                    title: Text(
+                                      friend["displayName"],
+                                      textScaler: TextScaler.linear(provider(context).tsf),
+                                    ),
+                                    subtitle: Text(
+                                      friend.id,
+                                      textScaler: TextScaler.linear(provider(context).tsf),
+                                    ),
+                                    trailing: IconButton(
+                                      icon: const Icon(Icons.delete),
+                                      tooltip: AppLocalizations.of(context)!.removeFriend,
+                                      onPressed: () async {
+                                        await Dio().post(
+                                          "$apiUrl/friend/remove",
+                                          queryParameters: {
+                                            "user": provider(context).user!.uid,
+                                            "removed": friend.id,
+                                          }
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
                 ),
               ),
             ],
