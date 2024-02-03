d8fa865a853e088d9fc534ff05c85692f5836b54
Panth Patel <panthhp@outlook.com>
2024-02-03 11:36:41 +0000

    matchmaking

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:tetraconnect/provider/app.settings.dart';
+
 import '../../ui/elements.dart';
+import '../../ui/theme.dart';
 import '../../util/route.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,11 +14,78 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool matchmaking = false;
+  int players = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: normalAppBar(context, route.home),
-      body: const Text("Home"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: !matchmaking
+              ? [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        matchmaking = true;
+                        players = 1;
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        AppLocalizations.of(context)!.startGame,
+                        textScaler: TextScaler.linear(provider(context).tsf),
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]
+              : [
+                  const Spacer(),
+                  defaultLoadingIndicator,
+                  Text(
+                    AppLocalizations.of(context)!.searchingForPlayers,
+                    textScaler: TextScaler.linear(provider(context).tsf),
+                    style: const TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    AppLocalizations.of(context)!.numberOfPlayers(players),
+                    textScaler: TextScaler.linear(provider(context).tsf),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    child: Text(
+                      AppLocalizations.of(context)!.cancel,
+                      style: TextStyle(
+                        color: theme.red.colour,
+                      ),
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        matchmaking = false;
+                        players = 0;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 32.0),
+                ],
+        ),
+      ),
     );
   }
 }
