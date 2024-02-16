86e98f70ddea03af08bf301e7e0d5413c984d307
Panth Patel <panthhp@outlook.com>
2024-02-16 12:09:05 +0000

    play game

---
+++ lib/pages/home/game.play.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:tetraconnect/util/api.dart';
+
+import '../../ui/elements.dart';
+import '../../util/route.dart';
+
+class GamePlayPage extends StatefulWidget {
+  final String gameId;
+
+  const GamePlayPage({super.key, required this.gameId});
+
+  @override
+  State<GamePlayPage> createState() => _GamePlayPageState();
+}
+
+class _GamePlayPageState extends State<GamePlayPage> {
+  int currentPlayerIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: normalAppBar(context, route.home),
+      body: StreamBuilder(
+        stream: firestore.doc("games/${widget.gameId}").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) => asyncBuilder(
+          context,
+          snapshot,
+          (game) {
+            if (!game["isPlaying"]) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                Navigator.of(context).pop();
+              });
+            }
+            return blank;
+          },
+        ),
+      ),
+    );
+  }
+}
